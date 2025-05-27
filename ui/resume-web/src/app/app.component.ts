8fa4363aa11dc28502d59380b7ff6cfc738e5f8e
Alexander Garbuz <alexander_garbuz@homedepot.com>
2025-05-27 17:18:20 -0500

    Added profile photo to header

--- ui/resume-web/src/app/app.component.ts
+++ ui/resume-web/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ProfilePhotoComponent } from './components/profile-photo/profile-photo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet,RouterModule, ProfilePhotoComponent],
   standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
