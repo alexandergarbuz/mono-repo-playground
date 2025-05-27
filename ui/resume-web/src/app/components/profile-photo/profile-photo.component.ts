b5ca6c9902dd4167e3d303510df4d380825c8336
Alexander Garbuz <alexander_garbuz@homedepot.com>
2025-05-27 17:15:10 -0500

    Updated profile photo component

--- ui/resume-web/src/app/components/profile-photo/profile-photo.component.ts
+++ ui/resume-web/src/app/components/profile-photo/profile-photo.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-profile-photo',
+  selector: 'profile-photo',
   standalone: true,
   imports: [],
   templateUrl: './profile-photo.component.html',
   styleUrl: './profile-photo.component.css'
 })
 export class ProfilePhotoComponent {
-  @Input() src:string = "'/images/profile-photo.jpg'"
-  @Input() alt:string = 'Alexander Garbuz Photo';
-  @Input() size:number = 150;
+  imgSrc = "/images/profile-photo.jpg"
+  @Input() imgId:string = "";
+  @Input() imgAlt:string = "Profile Photo";
+  @Input() imgSize:number = 150;
 
 }
